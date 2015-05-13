4c227f397ee40035917848969f7170e5fa6a2dc3
mprost <mprost9@aol.com>
2015-05-12 22:58:16 -0500

    Added Matt Print Statement
    This is a streamlined print statement that doesn't add any new lines.

--- Reflector.java
+++ Reflector.java
@@ -2,7 +2,7 @@ public Reflector{
   public static void main(String[] args)
   {
     System.out.println("Hello Zach");
-    System.out.println("Hello Jake");
+    System.out.println("Hello Jake\nHello Matt");
   }
 }
 //LETS SEE IF THIS WORKS?
