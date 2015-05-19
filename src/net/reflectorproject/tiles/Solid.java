ce7528b319b4a48bf61bc46c21c779c22014a877
Colonel Miles <zacharyckearney@gmail.com>
2015-05-18 21:44:48 -0500

    Current build 5/18 Solid
    Current build 5/18 added Solid class implements blockable

---
+++ src/net/reflectorproject/tiles/Solid.java
@@ -0,0 +1,25 @@
+package net.reflectorproject.tiles;
+
+	
+	public Solid() {
+		this((byte)0,(byte)0,false,null,(byte)0);
+	}
+	
+	public Solid(byte x, byte y) {
+		this(x,y,false,null,(byte)0);
+	}
+	
+	public Solid(byte x, byte y, boolean move) {
+		this(x,y,move,null,(byte)0);
+	}
+
+	public Solid(byte x, byte y, boolean move, String src) {
+		this(x,y,move,src,(byte)0);
+	}
+	
+	public Solid(byte x, byte y, boolean move, String src, byte direct) {
+		super(x, y, move, src, direct);
+	}
+
+}
+
