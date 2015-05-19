83a3c4265322105898ad1893552a8ea425d9ff3a
Rawhide <jakerowland@verizon.net>
2015-05-18 22:09:07 -0500

    Renamed Tiles to Tile
    From now on lets keep class files singular.

--- src/net/reflectorproject/tiles/Tile.java
+++ src/net/reflectorproject/tiles/Tile.java
@@ -3,6 +3,7 @@
  */
 package net.reflectorproject.tiles;
 
+import net.reflectorproject.world.Grid;
 import android.graphics.Picture;
 
 /**
@@ -12,7 +13,7 @@ import android.graphics.Picture;
  * @author Colonel Miles
  * 
  */
-public abstract class Tiles
+public abstract class Tile
 {
 	private enum Direction
 	{
@@ -46,28 +47,28 @@ public abstract class Tiles
 	
 	private Direction direction;
 	
-	public Tiles()
+	public Tile()
 	{
 		//X and Y position in the grid, Movable to false, and texture to null
 		this((byte)0,(byte)0,false,null,(byte)0);
 	}
 	
-	public Tiles(byte x, byte y)
+	public Tile(byte x, byte y)
 	{
 		this(x,y,false,null,(byte)0);
 	}
 	
-	public Tiles(byte x, byte y, boolean move)
+	public Tile(byte x, byte y, boolean move)
 	{
 		this(x,y,move,null,(byte)0);
 	}
 	
-	public Tiles(byte x, byte y, boolean move, String src)
+	public Tile(byte x, byte y, boolean move, String src)
 	{
 		this(x,y,move,src,(byte)0);
 	}
 	
-	public Tiles(byte x, byte y, boolean move, String src, byte direct)
+	public Tile(byte x, byte y, boolean move, String src, byte direct)
 	{
 		setxPosition(x);
 		setyPosition(y);
@@ -96,6 +97,8 @@ public abstract class Tiles
 				break;
 		}
 	}
+	
+	public abstract void update(Grid world);
 	
 	/**
 	 *@param xPosition the xPosition to set
